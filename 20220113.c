7aa41d6af3b46e2aaa33e52d992ea95e53026330
minsu kim <2874024@naver.com>
2022-01-13 22:48:42 +0900

    first commit

---
+++ 20220113.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	
+	printf("%s \n","----------------------");
+	printf("%-8s","나이");
+	printf("%-8s","전화");
+	printf("%6s\n\n","학점");
+	printf("%s \n","----------------------");
+	printf("%-8d%-8d%6.1lf",23,2461973,4.5);
+	return 0;
+}
+
+
+
+
+
+
+
+/*	printf("%10s%10s%10s\n\n","item", "count","price");
+	printf("%10s%10d%10d\n\n","pen", 20, 100);
+	printf("%10s%10d%10d\n\n","note", 5, 95);
+	printf("%10s%10d%10d\n\n","eraser", 110, 97);
+
+	*/
